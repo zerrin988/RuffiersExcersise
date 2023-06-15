2b95388a3899c44b777098441c674c0c1661d796
zerrin988 <zerrinulhaq@outlook.com>
2023-06-15 18:42:15 +0200

    commiting last screen 2

---
+++ FinalWin.py
@@ -0,0 +1,16 @@
+from PyQt5.QtCore import Qt, QTimer, QTime
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QLineEdit
+from Variables import *
+
+class FinalWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.UI()
+        self.connect()
+        self.show()
+    
+    def set_appear(self):
+        self.setWindowTitle(textFw)
+        self.resize(winW, winH)
+        self.move(winX, winY)
